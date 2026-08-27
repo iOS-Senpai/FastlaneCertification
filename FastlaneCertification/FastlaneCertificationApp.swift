fadfbed3b4c659a9feffbbafa9fb89c000eab4d0
Pradeep Kumar <mukku.kumar@telsuagcg.com>
2026-08-27 18:17:10 +0530

    first commit

---
+++ FastlaneCertification/FastlaneCertificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastlaneCertificationApp.swift
+//  FastlaneCertification
+//
+//  Created by xavient on 8/27/26.
+//
+
+import SwiftUI
+
+@main
+struct FastlaneCertificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
